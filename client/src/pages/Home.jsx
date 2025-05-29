9bca4b576c6b04b7815788f115e0ef7993a1f73b
Priyansh <priyanshkh14@gmail.com>
2025-05-29 12:52:45 +0530

    Fix: Added client directory as normal folder, updated Navbar

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import style from 'styled-components'
+
+const Container = style.div`
+  heihjht: 100%;
+  overflow-y: scroll;`
+
+const Home = () => {
+  return (
+    <Container>Home</Container>
+  )
+}
+
+export default Home
